b529267e1aa66dc64ba76c69e010b213dcfdb3d3
szhang58 <szhang58@u.rochester.edu>
2020-12-29 16:54:43 +0800

    initial commit, add features.

--- src/DFA.java
+++ src/DFA.java
@@ -1,2 +1,98 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+
 public class DFA {
+
+    int nstates;
+    int[] accepting;
+    int[][] transition;
+    char[] alphabet;
+    int currentState;
+
+
+    public boolean execute(String s){
+        int state = 0;
+        for(int i = 0; i < s.length(); i++){
+            try {
+                state = transition[state][s.charAt(i)];
+//                System.out.println(state);
+            }catch (Exception e){
+                state = -1;
+                break;
+            }
+        }
+        currentState = state;
+        return accept();
+    }
+
+    public void setTransition(int srcState, char c, int dstState){
+        transition[srcState][c] = dstState;
+    }
+
+    public boolean accept(){
+        for(int n: accepting){
+            if(n == currentState){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public DFA(int nstates){
+        alphabet = new char[128];
+        this.nstates = nstates;
+        transition = new int[nstates][alphabet.length];
+        for(int[] arr: transition){
+            Arrays.fill(arr, -1);
+        }
+    }
+
+    public int getNstates() {
+        return nstates;
+    }
+
+    public void setNstates(int nstates) {
+        this.nstates = nstates;
+    }
+
+    public int[] getAccepting() {
+        return accepting;
+    }
+
+    public void setAccepting(int[] accepting) {
+        this.accepting = accepting;
+    }
+
+    public int[][] getTransition() {
+        return transition;
+    }
+
+    public void setTransition(int[][] transition) {
+        this.transition = transition;
+    }
+
+    public char[] getAlphabet() {
+        return alphabet;
+    }
+
+    public void setAlphabet(char[] alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public static void main(String[] args){
+        int[] accepting = {0};
+        DFA dfa = new DFA(4);
+        dfa.setAccepting(accepting);
+        dfa.setTransition(0, '0', 3);
+        dfa.setTransition(0, '1', 1);
+        dfa.setTransition(1, '0', 2);
+        dfa.setTransition(1, '1', 0);
+        dfa.setTransition(2, '0', 1);
+        dfa.setTransition(2, '1', 3);
+        dfa.setTransition(3, '0', 0);
+        dfa.setTransition(3, '1', 2);
+
+    }
 }
---
+++ src/NFA.java
@@ -0,0 +1,207 @@
+import javafx.util.Pair;
+
+import java.util.*;
+
+public class NFA{
+
+    int nstates;
+    int[] accepting;
+    Set<Integer>[][] transition;
+    char[] alphabet;
+    Set<Integer> currentState;
+
+    Set<Character> alphabets;
+    public NFA(int nstates){
+        this.nstates = nstates;
+        alphabet = new char[128];
+        alphabets = new HashSet<>();
+        transition = new Set[nstates][alphabet.length];
+        for(Set<Integer>[] arr: transition){
+            Arrays.fill(arr, new HashSet<>());
+        }
+    }
+
+    public void setTransition(int srcState, char c, int dstState){
+        if(!transition[srcState][c].isEmpty()){
+            transition[srcState][c].add(dstState);
+        }else{
+            transition[srcState][c] = new HashSet<>();
+            transition[srcState][c].add(dstState);
+        }
+        alphabets.add(c);
+    }
+
+    public boolean execute(String s){
+        Set<Integer> set = new HashSet<>();
+        set.add(0);
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            Set<Integer> result = new HashSet<>();
+            for(int state: set){
+                result.addAll(transition[state][c]);
+            }
+            set = result;
+            currentState = set;
+        }
+        return accept();
+    }
+
+    public boolean accept(){
+        for(int n: accepting){
+            if(currentState.contains(n)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getNstates() {
+        return nstates;
+    }
+
+    public void setNstates(int nstates) {
+        this.nstates = nstates;
+    }
+
+    public int[] getAccepting() {
+        return accepting;
+    }
+
+    public void setAccepting(int[] accepting) {
+        this.accepting = accepting;
+    }
+
+    public Set[][] getTransition() {
+        return transition;
+    }
+
+    public void setTransition(Set[][] transition) {
+        this.transition = transition;
+    }
+
+    public char[] getAlphabet() {
+        return alphabet;
+    }
+
+    public void setAlphabet(char[] alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public static DFA NFAtoDFA(NFA nfa){
+        Map<Set<Integer>, Integer> map = new HashMap<>();
+
+        Set<Set<Integer>> buffer = new HashSet<>();
+
+        for(int i = 0; i < nfa.nstates; i++){
+            Set<Integer> singleton = new HashSet<>();
+            singleton.add(i);
+            map.put(singleton, map.size());
+            buffer.add(singleton);
+
+        }
+
+        Set<Set<Integer>> marker = new HashSet<>();
+
+        for(Set<Integer> state : buffer){
+            marker.add(state);
+            for(char c: nfa.alphabets) {
+                Set<Integer> result = new HashSet<>();
+                for(int n: state){
+                    result.addAll(nfa.transition[n][c]);
+                    if(!map.containsKey(result)){
+                        map.put(result, map.size());
+                        marker.add(result);
+                    }
+                }
+            }
+        }
+
+        Map<Pair<Set<Integer>, Character>, Set<Integer>> records = new HashMap<>();
+        while(!marker.isEmpty()){
+
+            buffer = new HashSet<>();
+            buffer.addAll(marker);
+            for(Set<Integer> state: buffer){
+                String s = state.toString();
+                for(char c: nfa.alphabets){
+                    Set<Integer> result = new HashSet<>();
+                    for(int n: state){
+                        result.addAll(nfa.transition[n][c]);
+                    }
+
+                    if(!map.containsKey(result)) {
+                        marker.add(result);
+                    }
+                    System.out.println("(" + s + ", " + c + ") = " + result);
+                    Pair<Set<Integer>, Character> pair = new Pair(state, c);
+                    records.put(pair, result);
+                }
+
+            }
+
+
+            for(Set<Integer> key: map.keySet()){
+                marker.remove(key);
+            }
+
+            for(Set<Integer> result: marker){
+                if(!map.containsKey(result)){
+                    map.put(result, map.size());
+                }
+            }
+        }
+
+        DFA dfa = new DFA(map.size());
+
+        for(Map.Entry<Pair<Set<Integer>, Character>, Set<Integer>> entry: records.entrySet()){
+            Pair<Set<Integer>, Character> pair = entry.getKey();
+
+            int srcState = map.get(pair.getKey());
+            char c = pair.getValue();
+            int dstState = map.get(records.get(pair));
+            dfa.setTransition(srcState, c, dstState);
+        }
+
+        List<Integer> accept = new ArrayList<>();
+        for(Set<Integer> set: map.keySet()){
+            for(int n: nfa.getAccepting()){
+                if(set.contains(n)){
+                    accept.add(map.get(set));
+                }
+            }
+        }
+
+        int[] accepting = new int[accept.size()];
+        for(int i = 0; i < accept.size(); i++){
+            accepting[i] = accept.get(i);
+        }
+        dfa.setAccepting(accepting);
+        return dfa;
+    }
+
+    public static void main(String[] args){
+        NFA nfa = new NFA(3);
+        int[] accpeting = {2};
+        nfa.setAccepting(accpeting);
+
+        nfa.setTransition(0, 'a', 0);
+        nfa.setTransition(0, 'a', 1);
+        nfa.setTransition(0, 'b', 0);
+        nfa.setTransition(0, 'c', 0);
+        nfa.setTransition(1, 'a', 1);
+        nfa.setTransition(1, 'b', 2);
+        nfa.setTransition(1, 'c', 0);
+        nfa.setTransition(2, 'a', 1);
+        nfa.setTransition(2, 'b', 0);
+        nfa.setTransition(2, 'c', 0);
+
+        DFA dfa = NFA.NFAtoDFA(nfa);
+        String[] words = {"asds", "asdab", "acab", "aba", "abcab"};
+        for(String word: words){
+            System.out.println(dfa.execute(word));
+        }
+
+
+    }
+
+}
